c2fec6f5b70e4b4974bf4706c1c8eaa80ba8f8f0
psoto-go <pabletesglez@gmail.com>
2021-06-11 16:36:49 +0000

    e

--- exercises/03.3-component-properties/index.js
+++ exercises/03.3-component-properties/index.js
@@ -19,8 +19,7 @@ const BootstrapCard = props => {
 };
 BootstrapCard.propTypes = {
 	title: PropType.string
-		// 2) add here the new properties into the proptypes object
+	// 2) add here the new properties into the proptypes object
 };
-
 
 // 3) Use ReactDOM to add the component into then DOM element #myDiv
